03ff2897b273c402629bb99228e720be762e867f
EmagesGroup <jgiron@emagesgroup.com>
2026-07-12 21:49:14 -0500

    Update preguntas.js

--- js/preguntas.js
+++ js/preguntas.js
@@ -1,1 +1,22 @@
+const preguntas = [
 
+{
+id:1,
+tipo:"multiple",
+competencia:"Empatía",
+nivel:"Intermedio",
+
+pregunta:"¿Cuál es el objetivo principal de la fase Empatizar dentro del Design Thinking?",
+
+opciones:[
+"Comprender profundamente las necesidades del usuario.",
+"Construir un prototipo funcional.",
+"Elegir la mejor campaña de marketing.",
+"Definir el presupuesto del proyecto."
+],
+
+respuesta:0
+
+},
+
+];
