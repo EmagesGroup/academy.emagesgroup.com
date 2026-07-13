7eadaf766fcd0e585d23397d8af36cc904d3d733
EmagesGroup <jgiron@emagesgroup.com>
2026-07-12 22:03:08 -0500

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,43 @@
+let tiempoRestante = 60 * 60;
 
+let intervaloTimer = null;
+
+function iniciarTemporizador(){
+
+if(intervaloTimer!=null)return;
+
+actualizarReloj();
+
+intervaloTimer=setInterval(function(){
+
+tiempoRestante--;
+
+actualizarReloj();
+
+if(tiempoRestante<=0){
+
+clearInterval(intervaloTimer);
+
+alert("El tiempo ha finalizado. La evaluación será enviada.");
+
+calcularResultado();
+
+}
+
+},1000);
+
+}
+
+function actualizarReloj(){
+
+let minutos=Math.floor(tiempoRestante/60);
+
+let segundos=tiempoRestante%60;
+
+document.getElementById("tiempo").innerText=
+
+String(minutos).padStart(2,"0")+":"+
+
+String(segundos).padStart(2,"0");
+
+}
